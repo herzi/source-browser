3febf27cf961be7324e3b59239c674173ebf730b
Sven Herzberg <sven@imendio.com>
2007-10-22 21:17:11 +0200

    Added a file chooser button

--- sb-main.c
+++ sb-main.c
@@ -23,19 +23,31 @@
 
 #include <gtk/gtk.h>
 
+#include <glib/gi18n.h>
+
 int
 main (int   argc,
       char**argv)
 {
 	GtkWidget* window;
+	GtkWidget* chooser;
 
 	gtk_init (&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 	g_signal_connect (window, "destroy",
 			  G_CALLBACK (gtk_main_quit), NULL);
-	gtk_widget_show_all (window);
+	gtk_window_set_default_size (GTK_WINDOW (window),
+				     400, 300);
+	gtk_window_set_title        (GTK_WINDOW (window),
+				     _("Source Browser"));
 
+	chooser = gtk_file_chooser_button_new (_("Choose File"),
+					       GTK_FILE_CHOOSER_ACTION_OPEN);
+	gtk_container_add (GTK_CONTAINER (window),
+			   chooser);
+
+	gtk_widget_show_all (window);
 	gtk_main ();
 	return 0;
 }
